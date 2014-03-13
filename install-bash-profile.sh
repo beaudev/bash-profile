f4065414297f61a7aee1d13359d2656b30294168
jmeyo <jc@poumtchi.com>
2014-03-13 15:14:46 +0100

    Integrate Gist scripts

--- install-bash-profile.sh
+++ install-bash-profile.sh
@@ -1,19 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 PROFILE_REPO="https://github.com/beaudev/bash-profile.git"
-PROFILE_PATH=".bash-profile.d"
-BASH_PROFILE=".bash_profile"
 
-[ ! type -p git ]&&  echo "'git' command not found. Please install git first." && exit 1
+source bash-profile.conf
+
+command -v git >/dev/null 2>&1  || ( echo "'git' command not found. Please install git first." && exit 1 )
 
 cd "$HOME"
-git clone ${PROFILE_REPO} ${PROFILE_PATH}
+[ ! -d ${PROFILE_PATH} ] && git clone ${PROFILE_REPO} ${PROFILE_PATH} || (cd ${PROFILE_PATH} && git pull)
+[ ! -d ${MY_PROFILE_PATH} ] && mkdir ${MY_PROFILE_PATH}
 
 if [ -f ${BASH_PROFILE} -a ! -f ${BASH_PROFILE}.default ]; then
   echo "Moving current ${BASH_PROFILE} to ${PROFILE_PATH}/${BASH_PROFILE}.default"
-  mv ${BASH_PROFILE} ${PROFILE_PATH}/${BASH_PROFILE}.default
+  mv ${BASH_PROFILE} ${BASH_PROFILE}.default
 fi
 
-ln -s PROFILE_PATH .bash_profile
-
 echo "source ~/${PROFILE_PATH}/bash-profile" > ${BASH_PROFILE}
+[ -f ${BASH_PROFILE}.default ] && echo "source ${BASH_PROFILE}.default" >> ${BASH_PROFILE}
+
+echo "bash-profile has been setup"
