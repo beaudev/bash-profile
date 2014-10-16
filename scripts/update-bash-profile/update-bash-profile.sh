3dba6c05ef874cb3226d5aeec085909439f8c439
jc <jc@houseofagile.com>
2014-10-16 17:27:38 +0200

    Init submodule and update submodule when refreshing bash-profile

--- scripts/update-bash-profile/update-bash-profile.sh
+++ scripts/update-bash-profile/update-bash-profile.sh
@@ -5,7 +5,7 @@ PROFILE_REPO="https://github.com/beaudev/bash-profile.git"
 update-bash-profile(){
     (
     cd $HOME
-    [ ! -d ${PROFILE_PATH} ] && git clone ${PROFILE_REPO} ${PROFILE_PATH} || (cd ${PROFILE_PATH} && git pull)
+    [ ! -d ${PROFILE_PATH} ] && { git clone ${PROFILE_REPO} ${PROFILE_PATH};cd ${PROFILE_PATH}; git submodule init ;git submodule update; } || (cd ${PROFILE_PATH} && git pull && git submodule update)
     echo "bash-profile has been updated"
     )
 
